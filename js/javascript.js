c6510049f9e0e25fa7dd16db8642cb180c848264
RafaelaEssences <theessencestarot@gmail.com>
2023-09-16 17:00:36 -0300

    mexendo no javascript

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var rad = document.querySelectorAll('input[type=radio]');
+    console.log(rad);
+    
+});
+
+function EscolhaOpcao() {
+    //var radio = document.querySelector('input[name=rdmensagens]:checked').value;
+
+    var rad = document.querySelectorAll('input[type=radio]');
+
+    for (var i = 0; i<= rad.length; i++)
+    {
+
+    }
+    //console.log('oiiii')
+
+
+}
