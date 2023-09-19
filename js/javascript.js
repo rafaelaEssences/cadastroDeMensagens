e07a44e80327ea6b58daba9ad3fa673ade51dd8f
RafaelaEssences <theessencestarot@gmail.com>
2023-09-19 00:05:17 -0300

    Geração do item de remarcação, geração do modal alerta da data

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,61 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("fieldAvisos").style.display = 'none';
+  document.getElementById("fieldAvisos").style.display = 'none';
+  document.getElementById("fieldRemarcacao").style.display = 'none';
+  //document.getElementById('datepicker').style.border = "none";
+
+  // document.getElementById('datepicker').addEventListener('change', function(){alert('test');});
 
 });
 
 
-function TrocaDeOpcao(){
+function TrocaDeOpcao() {
+
+  document.getElementById("fieldAvisos").style.display = 'none';
+
+  var radios = document.getElementsByName("rdmensagens");
+  var nome = document.getElementById("inputNome").textContent;
+  var opcao = null;
+  var texto = null;
+
+  for (var i = 0; i < radios.length; i++) {
+
+    if (radios[i].checked) {
+
+      opcao = radios[i].value;
+      break;
+    }
+  }
+  if (opcao == "vaga") {
 
     document.getElementById("fieldAvisos").style.display = 'none';
-    
+    document.getElementById("fieldRemarcacao").style.display = 'none';
+    texto = document.getElementById("textoDaMensagem").textContent = CriacaoDaMensagemDeVaga();
+  }
+  else if (opcao == "remarcacao") {
+    document.getElementById("fieldAvisos").style.display = 'none';
+    document.getElementById("fieldRemarcacao").style.display = 'block';
+  }
+  else {
+    document.getElementById("fieldAvisos").style.display = 'block';
+    document.getElementById("fieldRemarcacao").style.display = 'none';
+    texto = document.getElementById("textoDaMensagem").textContent = CriacaoDeMensagemDeAviso();
+  }
 
-    var radios = document.getElementsByName("rdmensagens");
-    var nome = document.getElementById("inputNome").textContent;
-    var opcao = null;
-    var texto = null;
-
-    for (var i = 0; i < radios.length; i++) {
-
-      if (radios[i].checked) {
-
-
-        opcao = radios[i].value;
-        break;
-      }
-    }
-    if (opcao == "vaga") {
-      //console.log("opção escolhida foi vaga");
-      //document.getElementById("textoDaMensagem").value  = "";
-      texto = document.getElementById("textoDaMensagem").textContent = CriacaoDaMensagemDeVaga();
-    }
-    else if (opcao == "remarcacao") {
-      //console.log("opção escolhida foi remarcacao");
-    }
-    else {
-       //console.log("opção escolhida foi vaga");
-       //document.getElementById("textoDaMensagem").value  = "";
-       document.getElementById("fieldAvisos").style.display = 'block';
-       texto = document.getElementById("textoDaMensagem").textContent = CriacaoDeMensagemDeAviso();
-    }
-
-    return texto;
+  return texto;
 
 }
 
 
-function CriadorDeMensagem() {
+function GerarTexto() {
+
+  if (document.getElementById('datepicker').value == '') {
+
+    //document.getElementById('datepicker').style.border = "2px solid red";
+    $("#modal-mensagem").modal();
+  }
+  else {
 
     var radios = document.getElementsByName("rdmensagens");
     var nome = document.getElementById("inputNome").textContent;
@@ -55,8 +65,6 @@ function CriadorDeMensagem() {
     for (var i = 0; i < radios.length; i++) {
 
       if (radios[i].checked) {
-
-
         opcao = radios[i].value;
         break;
       }
@@ -66,38 +74,65 @@ function CriadorDeMensagem() {
       texto = document.getElementById("textoDaMensagem").textContent = CriacaoDaMensagemDeVaga();
     }
     else if (opcao == "remarcacao") {
+      document.getElementById("fieldAvisos").style.display = 'none';
+      document.getElementById("fieldRemarcacao").style.display = 'block';
+      texto = document.getElementById("textoDaMensagem").textContent = CriacaoDeMensagemDeRemarcacao();
     }
     else {
-       
-       document.getElementById("fieldAvisos").style.display = 'block';
-       texto = document.getElementById("textoDaMensagem").textContent = CriacaoDeMensagemDeAviso();
+
+      document.getElementById("fieldAvisos").style.display = 'block';
+      document.getElementById("fieldRemarcacao").style.display = 'none';
+      texto = document.getElementById("textoDaMensagem").textContent = CriacaoDeMensagemDeAviso();
     }
 
     return texto;
   }
+}
+
+function CriacaoDeMensagemDeAviso() {
+
+  var texto = null;
+  var nome = document.querySelector('input[id="inputNome"]').value;
+
+  var getHora = document.getElementById("selectHoras");
+  var hora = getHora.options[getHora.selectedIndex].text;
+
+  var getMinutos = document.getElementById("selectMinutos");
+  var minutos = getMinutos.options[getMinutos.selectedIndex].text;
+
+  var dataPorExtenso = document.querySelector('label[id="lblDataExtensoTexto"]').textContent;
+  var diaDaSemana = dataPorExtenso.split(',')[0];
+  var data = dataPorExtenso.split(',')[1];
+
+  texto = TextoDeAvisos(diaDaSemana, hora, minutos, data, nome);
+
+  return texto;
+}
 
 
-  function CriacaoDaMensagemDeVaga() {
+function CriacaoDeMensagemDeRemarcacao() {
 
-    var texto = null;
-    var nome = document.querySelector('input[id="inputNome"]').value;
-    
-    var getHora = document.getElementById("selectHoras");
-    var hora = getHora.options[getHora.selectedIndex].text;
+  var texto = null;
+  var nome = document.querySelector('input[id="inputNome"]').value;
 
-    var getMinutos = document.getElementById("selectMinutos");
-    var minutos = getMinutos.options[getMinutos.selectedIndex].text;
+  var getHora = document.getElementById("selectHoras");
+  var hora = getHora.options[getHora.selectedIndex].text;
 
-    var dataPorExtenso = document.querySelector('label[id="lblDataExtensoTexto"]').textContent;
-    var diaDaSemana =  dataPorExtenso.split(',')[0];
-    var data =  dataPorExtenso.split(',')[1];
+  var getMinutos = document.getElementById("selectMinutos");
+  var minutos = getMinutos.options[getMinutos.selectedIndex].text;
 
-    //texto = TextoDeVagas(diaDaSemana, hora, minutos, data);
+  var dataPorExtenso = document.querySelector('label[id="lblDataExtensoTexto"]').textContent;
+  var diaDaSemana = dataPorExtenso.split(',')[0];
+  var data = dataPorExtenso.split(',')[1];
 
+  texto = "*Olá, " + nome + ", você foi remarcado(a) para o dia" + data + ", " + diaDaSemana + ", às " + hora + minutos + " (horário de Brasília - Brasil)*!";
 
-    texto = "*Olá, " + nome + ", você foi marcado(a) para o dia" + data + ", " + diaDaSemana + ", às " + hora +  minutos +  " (horário de Brasília - Brasil)*!";
+  if (document.querySelector("input[id='chckConfirmacaoDiasAntes']").checked == true) {
     texto += "\n\n";
     texto += "❗️ *Eu sempre confirmo poucos dias antes a consulta pelo whatsapp, por favor, é necessário que fique atento(a)!*"
+  }
+
+  if (document.querySelector("input[id='chckPagamentoRemarcacao']").checked == true) {
     texto += "\n\n";
     texto += "☀️ Poderia me confirmar abaixo a forma de pagamento?";
     texto += "\n";
@@ -106,96 +141,147 @@ function CriadorDeMensagem() {
     texto += "2) Transferência bancária (no máximo 3 dias úteis antes da consulta).";
     texto += "\n";
     texto += "3) Paypal (só pra quem está fora do Brasil -  2 dias úteis antes da consulta).";
+  }
+  if (document.querySelector("input[id='chckContinueAdiantandoRemarcacao']").checked == true) {
     texto += "\n\n";
-    texto+= "❗️ *Obs: Conforme for 'vagando' eu vou te passando pra frente, ok? Habilite as notificações e fique em alerta!*"
-    texto += "\n\n";
-    texto += "Obrigada!";
-    return texto;
+    texto += "❗️ *Obs: Conforme for 'vagando' eu vou te passando pra frente, ok? Habilite as notificações e fique em alerta!*"
   }
 
-  function CriacaoDeMensagemDeAviso() {
+  texto += "\n\n";
+  texto += "Obrigada!";
+  return texto;
 
-    var texto = null;
-    var nome = document.querySelector('input[id="inputNome"]').value;
-    
-    var getHora = document.getElementById("selectHoras");
-    var hora = getHora.options[getHora.selectedIndex].text;
+}
 
-    var getMinutos = document.getElementById("selectMinutos");
-    var minutos = getMinutos.options[getMinutos.selectedIndex].text;
 
-    var dataPorExtenso = document.querySelector('label[id="lblDataExtensoTexto"]').textContent;
-    var diaDaSemana =  dataPorExtenso.split(',')[0];
-    var data =  dataPorExtenso.split(',')[1];
+function CriacaoDaMensagemDeVaga() {
 
-    texto = TextoDeAvisos(diaDaSemana, hora, minutos,data, nome);
- 
-    return texto;
-  }
+  var texto = null;
+  var nome = document.querySelector('input[id="inputNome"]').value;
 
-  function TextoDeAvisos(diaDaSemana, hora, minutos, data){
+  var getHora = document.getElementById("selectHoras");
+  var hora = getHora.options[getHora.selectedIndex].text;
 
-    var texto = null;
-    texto = "Olá!";
+  var getMinutos = document.getElementById("selectMinutos");
+  var minutos = getMinutos.options[getMinutos.selectedIndex].text;
+
+  var dataPorExtenso = document.querySelector('label[id="lblDataExtensoTexto"]').textContent;
+  var diaDaSemana = dataPorExtenso.split(',')[0];
+  var data = dataPorExtenso.split(',')[1];
+
+
+  texto = "*Olá, " + nome + ", você foi marcado(a) para o dia" + data + ", " + diaDaSemana + ", às " + hora + minutos + " (horário de Brasília - Brasil)*!";
+  texto += "\n\n";
+  texto += "❗️ *Eu sempre confirmo poucos dias antes a consulta pelo whatsapp, por favor, é necessário que fique atento(a)!*"
+  texto += "\n\n";
+  texto += "☀️ Poderia me confirmar abaixo a forma de pagamento?";
+  texto += "\n";
+  texto += "1) Pix (pagamento somente no dia).";
+  texto += "\n";
+  texto += "2) Transferência bancária (no máximo 3 dias úteis antes da consulta).";
+  texto += "\n";
+  texto += "3) Paypal (só pra quem está fora do Brasil -  2 dias úteis antes da consulta).";
+  texto += "\n\n";
+  texto += "❗️ *Obs: Conforme for 'vagando' eu vou te passando pra frente, ok? Habilite as notificações e fique em alerta!*"
+  texto += "\n\n";
+  texto += "Obrigada!";
+  return texto;
+}
+
+
+function TextoDeRemarcacao(diaDaSemana, hora, minutos, data, nome) {
+
+  var texto = null;
+  texto = "*Olá, " + nome + ", você foi remarcado(a) para o dia" + data + ", " + diaDaSemana + ", às " + hora + minutos + " (horário de Brasília - Brasil)*!";
+  texto += "\n\n";
+  texto += "❗️ *Eu sempre confirmo poucos dias antes a consulta pelo whatsapp, por favor, é necessário que fique atento(a)!*"
+  texto += "\n\n";
+  texto += "☀️ Poderia me confirmar abaixo a forma de pagamento?";
+  texto += "\n";
+  texto += "1) Pix (pagamento somente no dia).";
+  texto += "\n";
+  texto += "2) Transferência bancária (no máximo 3 dias úteis antes da consulta).";
+  texto += "\n";
+  texto += "3) Paypal (só pra quem está fora do Brasil -  2 dias úteis antes da consulta).";
+  texto += "\n\n";
+  texto += "❗️ *Obs: Conforme for 'vagando' eu vou te passando pra frente, ok? Habilite as notificações e fique em alerta!*"
+  texto += "\n\n";
+  texto += "Obrigada!";
+  return texto;
+
+}
+
+function TextoDeAvisos(diaDaSemana, hora, minutos, data) {
+
+  var texto = null;
+  texto = "Olá!";
+  texto += "\n";
+  texto += "*Dia " + data + " (" + diaDaSemana + ", às " + hora + minutos + " - horário de Brasília - Brasil) surgiu uma vaga para consulta.*";
+  texto += "\n\n";
+  texto += "❗️ Você gostaria de adiantar o atendimento?";
+
+  if (document.querySelector("input[id='chckContinueAdiantandoAvisos']").checked == true) {
+    texto += "\n\n";
+    texto += "❗️ *OBS: conforme for 'vagando' eu vou continuar adiantando, ok?*";
     texto += "\n";
-    texto += "*Dia " + data +" ("+ diaDaSemana  + ", às " + hora +  minutos +" - horário de Brasília - Brasil) surgiu uma vaga para consulta.*";
-    texto += "\n\n";
-    texto += "❗️ Você gostaria de adiantar o atendimento?";
-
-    if(document.querySelector("input[id='chckContinueAdiantando']").checked == true){
-        texto += "\n\n";
-        texto += "❗️ *OBS: conforme for 'vagando' eu vou continuar adiantando, ok?*";
-        texto += "\n";
-    }
-    else{
-        texto += "\n\n"; 
-    }
-    texto += "❗️ *OBS: por favor, caso você não queira, poderia me informar?*";
-    texto += "\n\n";
-    texto += "Obrigada!";
-
-    return texto;
-
   }
-
-  function TextoDeVagas(diaDaSemana, hora, minutos, data, nome){
-
-    var texto = null;
-    texto = "*Olá, " + nome + ", você foi marcado(a) para o dia" + data + ", " + diaDaSemana + ", às " + hora +  minutos +  " (horário de Brasília - Brasil)*!";
+  else {
     texto += "\n\n";
-    texto += "❗️ *Eu sempre confirmo poucos dias antes a consulta pelo whatsapp, por favor, é necessário que fique atento(a)!*"
-    texto += "\n\n";
-    texto += "☀️ Poderia me confirmar abaixo a forma de pagamento?";
-    texto += "\n";
-    texto += "1) Pix (pagamento somente no dia).";
-    texto += "\n";
-    texto += "2) Transferência bancária (no máximo 3 dias úteis antes da consulta).";
-    texto += "\n";
-    texto += "3) Paypal (só pra quem está fora do Brasil -  2 dias úteis antes da consulta).";
-    texto += "\n\n";
-    texto+= "❗️ *Obs: Conforme for 'vagando' eu vou te passando pra frente, ok? Habilite as notificações e fique em alerta!*"
-    texto += "\n\n";
-    texto += "Obrigada!";
-    return texto;
-
   }
+  texto += "❗️ *OBS: por favor, caso você não queira, poderia me informar?*";
+  texto += "\n\n";
+  texto += "Obrigada!";
+
+  return texto;
+
+}
 
 
 
 
 
 
+function FormatarData() {
 
+  // if (document.getElementById('datepicker').value == '') {
+  //   document.getElementById('datepicker').style.border = "none";
+  // }
+}
 
-
-
-
-
-  function Limpar(){
-
+function TrocarData(){
+    if (document.getElementById('datepicker').value == '') {
+    document.getElementById('datepicker').style.border = "none";
   }
+  else{
+    document.getElementById('datepicker').style.border = "2px solid red";
+  }
+}
 
-  /* input dinamico */
+// document.getElementById("datepicker").addEventListener("change", function(){
+//   if (document.getElementById('datepicker').value == '') {
+//     document.getElementById('datepicker').style.border = "none";
+//   }
+//   else{
+//     document.getElementById('datepicker').style.border = "2px solid red";
+//   }
+// })
+  
+
+
+
+
+
+
+
+
+
+
+
+function Limpar() {
+
+}
+
+/* input dinamico */
 
 //   function buildTimePicker() {
 //     var result = document.createElement('span');
